aa27c363f6c955d3ef3b8a7f937e328b7ce07cc9
Michal Korybski <emkorybski@gmail.com>
2012-07-06 22:09:10 +0100

    Restaurant details basic view added and view returned to the user added

---
+++ AwesomeEnterpriseApp/AwesomeEnterpriseApp/Models/MovieSpots.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AwesomeEnterpriseApp.Models
+{
+    public class MovieSpots
+    {
+        public IEnumerable<SelectListItem> Spots { get; set; }
+    }
+}
